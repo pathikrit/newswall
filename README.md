10c9403ef123ea3dfee132e93d08ca91e6989b4e
pathikrit <pathikritbhowmick@msn.com>
2022-12-14 12:08:12 -0500

    convert pdf to png

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
+Inspired by the [e-ink newspaper](https://github.com/mmicire/nyt2png) project ([HN post](https://news.ycombinator.com/item?id=26611371));
+this is a pure node.js port of [graiz/newsprint](https://github.com/graiz/newsprint).
 
-Inspired by the [e-ink newspaper](https://www.gregraiz.com/i-made-an-eink-newspaper/) project.
-This is a node.js port of https://github.com/graiz/newsprint
-
-For installing and running the server, see [visionect.md](visionect.md).
+For installing and running the Visionect server, see [visionect.md](visionect.md).
 
 Running the Client:
 ```
 node app.js
 ```
 
-Configure the client
+Deploying:
+```
+
+```
+
+Configure the client on the server:
+```
+
+```
 
 
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dayjs = require('dayjs')
 const fs = require('fs')
 const Downloader = require('nodejs-file-downloader')
 const schedule = require('node-schedule')
+const pdf2img = require('pdf-img-convert')
 
 // Server configs
 const app = express()
@@ -11,40 +12,50 @@ const port = 3000
 // Newsstand configs
 const newsstand = './.newspapers' // Local directory to cache newspaper downloads
 const newspapers = ['WSJ', 'NY_NYT'] // See https://www.freedomforum.org/todaysfrontpages/ for list of papers
-const offsets = [0, 1, 2] // Try today, yesterday etc
-const refreshChron = '* * * * *' // Every hour
+const offsets = [0, 1, 2, 3] // Try today, yesterday etc
+const refreshChron = '0 * * * *' // Every hour check for new newspapers
 
 schedule.scheduleJob(refreshChron, downloadAll)
 
 function downloadAll() {
 	const today = dayjs()
-	for (const offset of offsets) {
-		for (const paper of newspapers) {
+	for (const offset of offsets)
+		for (const paper of newspapers)
 			download(paper, today.subtract(offset, 'days'))
-		}
-	}
 }
 
 function download(key, date) {
-	const yyyyMMdd = date.format('YYYY-MM-DD')
-	const directory = `${newsstand}/${yyyyMMdd}`
+	const path = [newsstand, date.format('YYYY-MM-DD'), `${key}.pdf`]
+	const debug = path.slice(1).join('/')
 
-	if (fs.existsSync(`${directory}/${key}.pdf`)) {
-		console.log(`Already downloaded ${key} for ${yyyyMMdd}`)
+	if (fs.existsSync(path.join('/'))) {
+		console.log(`Already downloaded ${debug}`)
 		return
 	}
 
-	console.log(`Downloading ${key} for ${yyyyMMdd} ...`)
+	console.log(`Checking for ${debug} ...`)
 	const downloader = new Downloader({
 		// e.g. https://cdn.freedomforum.org/dfp/pdf12/WSJ.pdf
-		url: `https://cdn.freedomforum.org/dfp/pdf${date.date()}/${key}.pdf`,
-		directory: directory,
+		url: `https://cdn.freedomforum.org/dfp/pdf${date.date()}/${path[2]}`,
+		directory: path.slice(0, 2).join('/'),
 		skipExistingFileName: true,
-		onBeforeSave: (deducedName) => {
-			console.log(`Saving ${yyyyMMdd}/${deducedName} ...`)
-		}
+		onBeforeSave: deducedName => console.log(`Saving ${path[1]}/${deducedName} ...`)
 	})
 	downloader.download()
+		.then(pdfToImage)
+		.catch(error => {
+			if (error.statusCode && error.statusCode === 404)
+				console.log(`${debug} is not available yet`)
+			else
+				console.error(`Could not download ${debug}`, error)
+		})
+}
+
+function pdfToImage(pdf) {
+	console.log(`Converting ${pdf.filePath} to png ...`)
+	pdf2img.convert(pdf.filePath).then(images => {
+		fs.writeFileSync(pdf.filePath.replace('.pdf', '.png'), images[0])
+	})
 }
 
 app.get('/', (req, res) => {
@@ -52,7 +63,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+	console.log(`Starting server on port ${port} ...`)
 })
 
 downloadAll()
