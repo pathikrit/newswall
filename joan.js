7af0b787f9a059a08f9256a829b0aed9890acd7e
pathikrit <pathikritbhowmick@msn.com>
2023-01-20 15:49:22 -0500

    move joan config to joan.js

--- joan.js
+++ joan.js
@@ -1,24 +1,33 @@
-const oauth2Lib = require('simple-oauth2');
-const axios = require('axios');
+// This is a standlone util to interact with my Visonect display via the Joan API: https://portal.getjoan.com/api/docs/
+// So that I can display battery level overlayed on the newspaper
+// Feel free to delete this file if you use some other e-ink display
 
-const config = require('./config')
+const config = {
+	client_id: 'EIFZqijAtYH6DOkgBJV2pthRFHoSmQOq3niiJfoi',
+	client_secret: 'gRlHMs35NchXe5fkfFiYTyVMXD5PXaKLURPNtlIMLgJm3Pp8YzfrygjEuDjvGAukS1UfNTYHiI7cIX6za2sKvB9gu2HMdtzyNATqgstqo0FdDRUrFwxU6OVzhYvans6v',
+	apiHost: 'https://portal.getjoan.com',
+	apiVersion: '1.0'
+}
 
 var accessToken = false
 function newAccessToken() {
-	const oauth2 = oauth2Lib.create(config.joan.oauth)
+	const oauth2 = require('simple-oauth2').create({
+		client: {id: config.client_id, secret: config.client_secret},
+		auth: {tokenHost: config.apiHost, tokenPath: '/api/token/'}
+	})
 	return oauth2.clientCredentials
 		.getToken({scope: 'read write'})
 		.then(result => oauth2.accessToken.create(result))
 }
 
-async function call(api, data) {
+async function call(path, data) {
 	if (!accessToken || accessToken.expired()) {
 		accessToken = await newAccessToken()
 		console.log(`Bearer ${accessToken.token.access_token}`)
 	}
-	const res = await axios({
+	const res = await require('axios')({
 		method: data ? 'POST' : 'GET',
-		url: config.joan.apiUrl + api + '/',
+		url: `${config.apiHost}/api/v${config.apiVersion}/${path}/`,
 		headers: {'Authorization': 'Bearer ' + accessToken.token.access_token},
 		data: data
 	})
@@ -26,5 +35,13 @@ async function call(api, data) {
 }
 
 module.exports = {
-	status: () => call('devices')[0]
+	status: () => call('devices').then(data => {
+		console.assert(data.count === 1, `Invalid # of devices found = ${data}`)
+		return data.results[0]
+	})
 }
+
+
+call('devices').then(data => {
+	console.log(data)
+})
