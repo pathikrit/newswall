090b6b2d97244f45dcf18197ac2510e53aaf2208
pathikrit <pathikritbhowmick@msn.com>
2023-02-27 17:21:16 -0500

    add more tests

--- app.test.js
+++ app.test.js
@@ -1,10 +1,15 @@
 const test = require('supertest')
 const app = require('./app')
+const {StatusCodes} = require('http-status-codes')
 
 describe('server', () => {
-	it("should serve homepage", () => test(app).get('/').expect(200))
-	it("should serve a paper", () => test(app).get('/latest').expect(200))
-	it("should serve NYT", () => test(app).get('/latest?papers=NYT').expect(200))
-	it("should serve any paper", () => test(app).get('/latest?papers=-1').expect(200))
-	it("serve 404 404", () => test(app).get('/foo').expect(404))
+	check = (msg, path, code = StatusCodes.OK) => it(`should ${code === StatusCodes.OK ? '' : 'not '}serve ${msg}`, () => test(app).get(path).expect(code))
+
+	check('homepage', '/')
+	check('latest', '/latest')
+	check('NYT', '/latest?papers=NYT')
+	check('any paper', '/latest?papers=-1')
+	check('a device', '/latest/2a002800-0c47-3133-3633-333400000000')
+	check('a missing device', '/latest/-1')
+	check('bad route', '/foo', StatusCodes.NOT_FOUND)
 })
