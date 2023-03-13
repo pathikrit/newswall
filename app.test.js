c700e2c7c85bc44ec22c1aa90f85e1d31a6c9b60
pathikrit <pathikritbhowmick@msn.com>
2023-03-13 17:48:11 -0400

    test DSL

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 require('jest-matcher-one-of')
 const puppeteer   = require('puppeteer')
 const portfinder  = require('portfinder')
-const {OK, NOT_FOUND} = require('http-status-codes').StatusCodes
+const {StatusCodes} = require("http-status-codes");
 
 jest.setTimeout(60*1000) // Initial download might be slow
 
@@ -22,28 +22,32 @@ contains = str => page => expect(page.content()).resolves.toContain(str)
 
 actualPaperOn = page => expect(page.$eval('img', el => el.id))
 
-displayingPaper = (...papers) => page => papers.length > 0 ? actualPaperOn(page).resolves.toBeOneOf(papers) : actualPaperOn(page).resolves.not.toBeNull()
+serves = (url, substring) => [StatusCodes.OK, url, contains(substring)]
 
-notDisplayingPaper = paper => page => paper ? actualPaperOn(page).resolves.not.toBe(paper) : actualPaperOn(page).rejects.toThrow()
+showsPaper = (url, papers) => [StatusCodes.OK, url, page => papers ? actualPaperOn(page).resolves.toBeOneOf(papers) : actualPaperOn(page).resolves.not.toBeNull()]
+
+doesNotShowPaper = (url, paper) => [StatusCodes.OK, url, page => paper ? actualPaperOn(page).resolves.not.toBe(paper) : actualPaperOn(page).rejects.toThrow()]
+
+doesNotServe = url => [StatusCodes.NOT_FOUND, url, contains(`Cannot GET ${url}`)]
 
 test.each([
-  [OK, '/', contains('emulator')],
-  [OK, '/archive', contains('archive')],
-  [OK, '/latest', displayingPaper()],
-  [OK, '/latest?prev=NYT', notDisplayingPaper('NYT')],
-  [OK, '/latest?papers=NYT', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT&prev=NYT', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT&prev=INVALID', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT,WSJ', displayingPaper('NYT', 'WSJ')],
-  [OK, '/latest?papers=NYT,WSJ&prev=NYT', displayingPaper('WSJ')],
-  [OK, '/latest?papers=NYT,WSJ&prev=WSJ', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT,INVALID', displayingPaper('NYT')],
-  [OK, '/latest?papers=INVALID', notDisplayingPaper()],
-  [OK, '/latest?deviceId=INVALID', notDisplayingPaper()],
-  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000', displayingPaper()],
-  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000&prev=WSJ', notDisplayingPaper('WSJ')],
-  [NOT_FOUND, '/latest/INVALID', contains('Cannot GET')],
-  [NOT_FOUND, '/INVALID', contains('Cannot GET')]
+  serves('/', 'emulator'),
+  serves('/archive', 'archive'),
+  showsPaper('/latest'),
+  doesNotShowPaper('/latest?prev=NYT', 'NYT'),
+  showsPaper('/latest?papers=NYT', ['NYT']),
+  showsPaper('/latest?papers=NYT&prev=NYT', ['NYT']),
+  showsPaper('/latest?papers=NYT&prev=INVALID', ['NYT']),
+  showsPaper('/latest?papers=NYT,WSJ', ['NYT', 'WSJ']),
+  showsPaper('/latest?papers=NYT,WSJ&prev=NYT', ['WSJ']),
+  showsPaper('/latest?papers=NYT,WSJ&prev=WSJ', ['NYT']),
+  showsPaper('/latest?papers=NYT,INVALID', ['NYT']),
+  doesNotShowPaper('/latest?papers=INVALID'),
+  doesNotShowPaper('/latest?deviceId=INVALID'),
+  showsPaper('/latest?deviceId=2a002800-0c47-3133-3633-333400000000'),
+  doesNotShowPaper('/latest?deviceId=2a002800-0c47-3133-3633-333400000000&prev=WSJ', ['WSJ']),
+  doesNotServe('/latest/INVALID'),
+  doesNotServe('/INVALID')
 ])('%i: %s', (statusCode, path, check) => browser.newPage().then(page => Promise.all([
     page.goto(`http://127.0.0.1:${port}${[path]}`).then(res => expect(res.status()).toBe(statusCode)),
     page.waitForNetworkIdle().then(_ => check(page))
