7d9737a5a72db05b55faa7d20037f784725d1bf5
pathikrit <pathikritbhowmick@msn.com>
2023-03-13 13:32:44 -0400

    enable all tests

--- app.test.js
+++ app.test.js
@@ -1,6 +1,6 @@
 const puppeteer   = require('puppeteer')
 const portfinder  = require('portfinder')
-const {OK, NotFound} = require('http-status-codes').StatusCodes
+const {OK, NOT_FOUND} = require('http-status-codes').StatusCodes
 
 jest.setTimeout(60*1000) // Initial download might be slow
 
@@ -17,33 +17,32 @@ afterAll(async () => {
   await server.close()
 })
 
-contains = (expected) => html => expected.some(part => html.includes(part)) ? Promise.resolve(html) : Promise.reject(`Did not find ${expected} in ${html}`)
-displayingPaper = (...papers) => contains(papers.map(paper => `<img id="${paper || ''}`))
-const any = null
+displayingPaper = (html, paper) => html.includes(`<img id="${paper || ''}`)
 
 test.each([
-  [OK, '/', contains(['emulator'])],
-  [OK, '/archive', contains(['archive'])],
-  [OK, '/latest', displayingPaper(any)],
-  [OK, '/latest', displayingPaper(any)],
-  [OK, '/latest?prev=NYT', !displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT&prev=NYT', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT&prev=INVALID', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT,WSJ', displayingPaper('NYT', 'WSJ')],
-  [OK, '/latest?papers=NYT,WSJ&prev=NYT', displayingPaper('WSJ')],
-  [OK, '/latest?papers=NYT,WSJ&prev=WSJ', displayingPaper('NYT')],
-  [OK, '/latest?papers=NYT,INVALID', displayingPaper('NYT')],
-  //shouldNotServe('/latest?papers=INVALID')
-  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000', displayingPaper(any)],
-  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000&prev=WSJ', !displayingPaper('WSJ')],
-  //shouldNotServe('/latest/INVALID')
-  //shouldNotServe('/INVALID')
+  [OK, '/', html => html.includes('emulator')],
+  [OK, '/archive', html => html.includes('archive')],
+  [OK, '/latest', displayingPaper],
+  [OK, '/latest?prev=NYT', html => !displayingPaper(html, 'NYT')],
+  [OK, '/latest?papers=NYT', html => displayingPaper(html, 'NYT')],
+  [OK, '/latest?papers=NYT&prev=NYT', html => displayingPaper(html, 'NYT')],
+  [OK, '/latest?papers=NYT&prev=INVALID', html => displayingPaper(html, 'NYT')],
+  [OK, '/latest?papers=NYT,WSJ', html => displayingPaper(html,'NYT') || displayingPaper(html,'WSJ')],
+  [OK, '/latest?papers=NYT,WSJ&prev=NYT', html => displayingPaper(html, 'WSJ')],
+  [OK, '/latest?papers=NYT,WSJ&prev=WSJ', html => displayingPaper(html, 'NYT')],
+  [OK, '/latest?papers=NYT,INVALID', html => displayingPaper(html,'NYT')],
+  [OK, '/latest?papers=INVALID', displayingPaper],
+  [OK, '/latest?deviceId=INVALID', displayingPaper],
+  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000', displayingPaper],
+  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000&prev=WSJ', html => !displayingPaper(html, 'WSJ')],
+  [NOT_FOUND, '/latest/INVALID', html => html.includes('Cannot GET')],
+  [NOT_FOUND, '/INVALID', html => html.includes('Cannot GET')]
 ])('%i: %s', async (statusCode, path, bodyCheck) => {
     const page = await browser.newPage()
     const res = await page.goto(`http://localhost:${port}${[path]}`)
     await page.waitForNetworkIdle()
+    expect(typeof(bodyCheck) === 'function')
     expect(res.status()).toBe(statusCode)
-    return page.content().then(bodyCheck)
+    return page.content().then(content => bodyCheck(content) ? Promise.resolve(content) : Promise.reject(content))
   }
 )
