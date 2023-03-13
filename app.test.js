099f6583172f78a6280aed76765f3c742a4e5440
pathikrit <pathikritbhowmick@msn.com>
2023-03-13 17:34:58 -0400

    fix tests

--- app.test.js
+++ app.test.js
@@ -1,3 +1,4 @@
+require('jest-matcher-one-of')
 const puppeteer   = require('puppeteer')
 const portfinder  = require('portfinder')
 const {OK, NOT_FOUND} = require('http-status-codes').StatusCodes
@@ -17,32 +18,33 @@ afterAll(async () => {
   await server.close()
 })
 
-displayingPaper = (html, paper) => html.includes(`<img id="${paper || ''}`)
+contains = str => page => expect(page.content()).resolves.toContain(str)
+
+actualPaperOn = page => expect(page.$eval('img', el => el.id))
+
+displayingPaper = (...papers) => page => papers.length > 0 ? actualPaperOn(page).resolves.toBeOneOf(papers) : actualPaperOn(page).resolves.not.toBeNull()
+
+notDisplayingPaper = paper => page => paper ? actualPaperOn(page).resolves.not.toBe(paper) : actualPaperOn(page).rejects.toThrow()
 
 test.each([
-  [OK, '/', html => html.includes('emulator')],
-  [OK, '/archive', html => html.includes('archive')],
-  [OK, '/latest', displayingPaper],
-  [OK, '/latest?prev=NYT', html => !displayingPaper(html, 'NYT')],
-  [OK, '/latest?papers=NYT', html => displayingPaper(html, 'NYT')],
-  [OK, '/latest?papers=NYT&prev=NYT', html => displayingPaper(html, 'NYT')],
-  [OK, '/latest?papers=NYT&prev=INVALID', html => displayingPaper(html, 'NYT')],
-  [OK, '/latest?papers=NYT,WSJ', html => displayingPaper(html,'NYT') || displayingPaper(html,'WSJ')],
-  [OK, '/latest?papers=NYT,WSJ&prev=NYT', html => displayingPaper(html, 'WSJ')],
-  [OK, '/latest?papers=NYT,WSJ&prev=WSJ', html => displayingPaper(html, 'NYT')],
-  [OK, '/latest?papers=NYT,INVALID', html => displayingPaper(html,'NYT')],
-  [OK, '/latest?papers=INVALID', displayingPaper],
-  [OK, '/latest?deviceId=INVALID', displayingPaper],
-  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000', displayingPaper],
-  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000&prev=WSJ', html => !displayingPaper(html, 'WSJ')],
-  [NOT_FOUND, '/latest/INVALID', html => html.includes('Cannot GET')],
-  [NOT_FOUND, '/INVALID', html => html.includes('Cannot GET')]
-])('%i: %s', async (statusCode, path, bodyCheck) => {
-    const page = await browser.newPage()
-    const res = await page.goto(`http://localhost:${port}${[path]}`)
-    await page.waitForNetworkIdle()
-    expect(typeof(bodyCheck) === 'function')
-    expect(res.status()).toBe(statusCode)
-    return page.content().then(content => bodyCheck(content) ? Promise.resolve(content) : Promise.reject(content))
-  }
-)
+  [OK, '/', contains('emulator')],
+  [OK, '/archive', contains('archive')],
+  [OK, '/latest', displayingPaper()],
+  [OK, '/latest?prev=NYT', notDisplayingPaper('NYT')],
+  [OK, '/latest?papers=NYT', displayingPaper('NYT')],
+  [OK, '/latest?papers=NYT&prev=NYT', displayingPaper('NYT')],
+  [OK, '/latest?papers=NYT&prev=INVALID', displayingPaper('NYT')],
+  [OK, '/latest?papers=NYT,WSJ', displayingPaper('NYT', 'WSJ')],
+  [OK, '/latest?papers=NYT,WSJ&prev=NYT', displayingPaper('WSJ')],
+  [OK, '/latest?papers=NYT,WSJ&prev=WSJ', displayingPaper('NYT')],
+  [OK, '/latest?papers=NYT,INVALID', displayingPaper('NYT')],
+  [OK, '/latest?papers=INVALID', notDisplayingPaper()],
+  [OK, '/latest?deviceId=INVALID', notDisplayingPaper()],
+  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000', displayingPaper()],
+  [OK, '/latest?deviceId=2a002800-0c47-3133-3633-333400000000&prev=WSJ', notDisplayingPaper('WSJ')],
+  [NOT_FOUND, '/latest/INVALID', contains('Cannot GET')],
+  [NOT_FOUND, '/INVALID', contains('Cannot GET')]
+])('%i: %s', (statusCode, path, check) => browser.newPage().then(page => Promise.all([
+    page.goto(`http://127.0.0.1:${port}${[path]}`).then(res => expect(res.status()).toBe(statusCode)),
+    page.waitForNetworkIdle().then(_ => check(page))
+])))
