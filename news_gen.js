8a2c0e6ad1f6ea4d831c7a81930fee87e25c11b4
pathikrit <pathikritbhowmick@msn.com>
2025-02-22 20:32:05 -0500

    prompt improvement

--- news_gen.js
+++ news_gen.js
@@ -4,29 +4,32 @@ const { z } = require('zod')
 const { zodResponseFormat } = require('openai/helpers/zod')
 require('dotenv').config()
 
-const llm = new OpenAI()
+const llm = new OpenAI(process.env.OPENAI_API_KEY)
 const search_engine = new Exa(process.env.EXA_API_KEY)
 
 const synthesize_news_prompt = (articles) => ({
     name: "synthesize_news",
     system: `
         You will be provided with a list of news articles of format {topic, title, url, publishedDate, author, text, highlights, image}.
-        You must synthesize these articles into a new array containing {topic, title, highlight, image_urls, text},
+        You must synthesize these articles into a new array containing {topic, title, highlight, images, text},
         because my input may contain the same information multiple times from different sources.
         Make sure each item in the output array pertains to a distinct piece of news
         (ok to repeat topics but the text must not overlap with other items in the output array in terms of what information it contains)
         Make sure your output array is sorted from most important news to least important news
-        The image_urls in the output must be list of images pertaining to the same article from the input sources (empty array if none)
+        The images in the output must be list of image urls pertaining to the same article from the input sources (empty array if none)
+        Make sure there are no duplicate images urls in the output array
+        Again, if 2 texts in the output array are tangentially similar, make sure to combine them into a single array item
+        This output would be used to render a newpapaer front page with a list of articles, headers, bylines, and images
     `,
     user: JSON.stringify(articles),
-    schema: z.object({articles: z.array(z.object({topic: z.string(), title: z.string(), highlight: z.string(), image_urls: z.array(z.string()), text: z.string()}))})
+    schema: z.object({articles: z.array(z.object({topic: z.string(), title: z.string(), highlight: z.string(), images: z.array(z.string()), text: z.string()}))})
 })
 
 const ask_llm = (query, mini=true) => llm.beta.chat.completions.parse({
     model: mini ? "gpt-4o-mini" : "gpt-4o",
     messages: [
-        {role: 'user',content: query.user},
-        {role: 'system',content: query.system}
+        {role: 'system',content: query.system},
+        {role: 'user',content: query.user}
     ],
     response_format: zodResponseFormat(query.schema, query.name),
 }).then(result => result.choices[0].message.parsed)
@@ -36,12 +39,11 @@ const news_search = (topic) => search_engine
     .then(response => response.results.map(res => ({topic, title: res.title, url: res.url, publishedDate: res.publishedDate, author: res.author, text: res.text, highlights: res.highlights, image: res.image})))
 
 const make_news = (topics) => Promise.all(topics.map(topic => news_search(topic)))
-    .then(articles => ask_llm(synthesize_news_prompt(articles.flat())))
-
+    .then(articles => ask_llm(synthesize_news_prompt(articles.flat()), mini=false))
 
 const main = async () => {
     const res = await make_news(["spacex", "bergen county"]);
-    console.log(res)
+    console.log(JSON.stringify(res, null, 2))
 }
 
 main()
